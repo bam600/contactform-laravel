d4c2fa0f0c97e112afa4f0ad3b5e1ea69e8a94e3
hitomi sasanuma <chitomi4866@icloud.com>
2025-08-25 11:32:16 +0900

    確認テスト用環境構築修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CategorySeeder::class);
+        $this->call(CategorySeeder::class);
 
         $this->call(ContactSeeder::class);
     }
